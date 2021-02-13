5fb05d02a31652f602e44b9e63c30f6330bae546
munnangi venkatakarthik <75118794+venkatakarthik29@users.noreply.github.com>
2021-02-13 11:46:56 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,29 @@
 //Write the program to print "Hello World !"
+#include<stdio.h>
+float input()
+{
+ float n;
+ printf("enter the values:");
+ scanf("%f",&n);
+ return n; 
+}
+float volume(float h,float d,float b)
+{
+ float volume;
+ volume = (((h*d*b)+(d/b))/3);
+ return volume;
+}
+void output(float n)
+{
+ printf("volume is %f",n);
+}
+int main()
+{
+float h,d,b,v;
+h = input()
+d = input()
+b = input()
+v = volume(h,d,b);
+output(v);
+return 0;
+}
